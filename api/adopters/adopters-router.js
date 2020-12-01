ca8965b2dc15418c44126ddf03898dcc75893c13
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-01 19:18:06 +0100

    wip

--- api/adopters/adopters-router.js
+++ api/adopters/adopters-router.js
@@ -22,6 +22,7 @@ router.get('/', (req, res) => {
     })
 });
 
+// this comes second and will never be used
 router.get('/', async (req, res) => {
   // 1- pull stuff from req
   const { query } = req
